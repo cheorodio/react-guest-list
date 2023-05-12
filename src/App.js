03b6585f7f5a623fe43e65e4f4b3f6cb403e8fda
cheorodio <cheorodio@gmail.com>
2023-05-12 15:50:52 +0200

    Add functions to update guest attendance status and to delete a guest

--- src/App.js
+++ src/App.js
@@ -1,35 +1,43 @@
 import './index.css';
 import { useEffect, useState } from 'react';
+import { AiOutlineCloseCircle } from 'react-icons/ai';
 import styles from './App.module.scss';
 
 export default function App() {
   const [guestList, setGuestList] = useState([]);
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   const baseUrl = 'http://localhost:4000';
 
   // /////////////////////////////////
-  // get all guests from api link
+  // get all guests
   useEffect(() => {
     async function getGuestList() {
       const response = await fetch(`${baseUrl}/guests`);
-      const alGuestsData = await response.json();
-      setGuestList([alGuestsData.results[0]]);
+      const allGuestsData = await response.json();
+      setGuestList(allGuestsData);
+      setIsLoading(false);
     }
     getGuestList().catch((error) => {
       console.log(error);
     });
   }, []);
 
-  // create a new guest and save to baseUrl
+  // /////////////////////////////////
+  // Creating a new guest using POST method
   async function newGuest() {
     const response = await fetch(`${baseUrl}/guests`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ firstName: firstName, lastName: lastName }),
+      body: JSON.stringify({
+        firstName: firstName,
+        lastName: lastName,
+        attending: false,
+      }),
     });
     const createdGuest = await response.json();
     return createdGuest;
@@ -50,39 +58,116 @@ export default function App() {
       event.preventDefault();
     }
   };
+
+  // //////////////////////////////////
+  // Updating a guest attendance status using PUT method
+  async function updateGuest(id) {
+    const response = await fetch(`${baseUrl}/guests/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ attending: true }),
+    });
+    const updatedGuest = await response.json();
+    return updatedGuest;
+  }
+
+  // //////////////////////////////////
+  // Deleting a guest using DELETE method
+  function handleDeleteGuest(id) {
+    const deleteGuest = async () => {
+      const response = await fetch(`${baseUrl}/guests/${id}`, {
+        method: 'DELETE',
+      });
+      const deletedGuest = await response.json();
+      setGuestList(deletedGuest);
+    };
+    deleteGuest().catch((error) => {
+      console.error(error);
+    });
+  }
+
+  useEffect(() => {
+    if (guestList.length > 0) {
+      setIsLoading(false);
+    }
+  }, [guestList]);
+
   // //////////////////////////////////
 
-  return (
-    <div className={styles.container}>
-      <div className={styles.box}>
-        <h1>Guest List</h1>
+  if (isLoading) {
+    return 'Loading ...';
+  } else {
+    return (
+      <div className={styles.pageContainer}>
+        <div className={styles.formContainer} data-test-id="guest">
+          <h1>Party Guest List</h1>
+          {/* Input */}
+          <form data-test-id="guest" onSubmit={handleSubmit}>
+            <label>
+              First name
+              <input
+                value={firstName}
+                placeholder="First name"
+                onKeyDown={handleEnter}
+                onChange={(event) => {
+                  setFirstName(event.currentTarget.value);
+                }}
+              />
+            </label>
+            <label>
+              Last name
+              <input
+                value={lastName}
+                placeholder="Last name"
+                onChange={(event) => {
+                  setLastName(event.currentTarget.value);
+                }}
+              />
+            </label>
+            <button>Add Guest</button>
+          </form>
+          {/* Output */}
+          <div className={styles.outputContainer} data-test-id="guest">
+            {guestList.map((guest) => {
+              return (
+                <div
+                  className={styles.guestContainer}
+                  key={`guest--${guest.id}`}
+                >
+                  <div>
+                    <input
+                      aria-label="Attending"
+                      type="checkbox"
+                      onChange={async () => {
+                        await updateGuest();
+                      }}
+                    />
+                    <span>
+                      {guest.attending === 'true'
+                        ? 'attending'
+                        : 'not attending'}
+                    </span>
+                  </div>
+                  <p>
+                    {guest.firstName} {guest.lastName}
+                  </p>
 
-        {/* Input */}
-        <form data-test-id="guest" onSubmit={handleSubmit}>
-          <label>
-            First name
-            <input
-              value={firstName}
-              placeholder="First name"
-              onKeyDown={handleEnter}
-              onChange={(event) => {
-                setFirstName(event.currentTarget.value);
-              }}
-            />
-          </label>
-          <label>
-            Last name
-            <input
-              value={lastName}
-              placeholder="Last name"
-              onChange={(event) => {
-                setLastName(event.currentTarget.value);
-              }}
-            />
-          </label>
-          <button>Add Guest</button>
-        </form>
+                  <button
+                    aria-label="Remove"
+                    onClick={() => {
+                      handleDeleteGuest(guest.id);
+                    }}
+                  >
+                    <AiOutlineCloseCircle className={styles.closeButtonIcon} />
+                  </button>
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
